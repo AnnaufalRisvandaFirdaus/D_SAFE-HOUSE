17c399e1d0be3c54da506f0c1fe91d3ea6cd6702
AnnaufalRisvandaFirdaus <annaufalfir1@gmail.com>
2021-06-23 20:39:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layout.template');
+Route::get('', function () {
+    // return view('layout.template');
 });
+
 // Route::get('register', function () {
 //     return view('register');
 // });
