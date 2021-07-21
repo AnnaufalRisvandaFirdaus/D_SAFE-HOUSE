1c8f19aca0eb383a90840866823322fcfaf52ee8
AnnaufalRisvandaFirdaus <annaufalfir1@gmail.com>
2021-07-21 22:47:33 +0700

    update model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,14 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
+    // public function allData();
+    // {
+    //     return DB::table('users')->get();
+    // }
     use HasFactory, Notifiable;
 
     /**
