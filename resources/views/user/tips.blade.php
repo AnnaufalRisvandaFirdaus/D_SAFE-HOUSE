04c4ae2de6d90bc20643fbbf642ab60fc394e6ba
Alga Saputra <68761934+AlgaSaputra@users.noreply.github.com>
2021-07-21 23:16:46 +0700

    menu tips

---
+++ resources/views/user/tips.blade.php
@@ -0,0 +1,13 @@
+@extends('user.home')
+
+@section('title','Tips')
+
+@section('content')
+
+<h1>Cara Menggunakan Aplikasi</h1>
+<br><h2>Fitur Profil</h2> digunakan untuk mengisi data diri agar mempermudah untuk mencari pekerjaan
+<br> 
+
+
+    
+@endsection
